4da839b4418fab93f793df304daf67cd943910ea
Jaewon Seo <byeol@hotmail.co.kr>
2017-01-09 20:00:00 +0900

    feat(angular-cli): generate an Angular project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FyeaWebPage } from './app.po';
+
+describe('fyea-web App', function() {
+  let page: FyeaWebPage;
+
+  beforeEach(() => {
+    page = new FyeaWebPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
